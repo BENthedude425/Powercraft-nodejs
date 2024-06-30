91e72c73d2b599540db6c28a86de5ef2ab05a96a
BENthedude425 <unconfigured@null.spigotmc.org>
2024-06-30 12:50:25 +0100

    login functionality

--- frontend/pages/login.jsx
+++ frontend/pages/login.jsx
@@ -1,9 +1,29 @@
 import {React} from "react";
+import "./main.css";
 
 export default function Plogin(){
     return(
-        <div>
-            login page
+        <div className="page">
+            <div className="container_1">
+                <div className="container_header">
+                    <div style={{display:"inline"}}><span><b>P</b>owercraft</span></div>
+                    
+                    <img src="../pictures/apple.png" className="logo"/>
+                </div>
+                
+                <form method="POST" action="http://localhost:8080/api/login">
+                    <label>Login to powercraft</label>
+                    <label>Username</label>
+                    <input type="text" name="username" placeholder="username"/>
+
+                    <label>Password</label>
+                    <input type="password" name="password" placeholder="password"/>
+
+                    <input type="submit" value="Create Account" className="submit"/>
+
+                    <div class="bottom"><a href="create-user">Create an account</a></div>
+                </form>
+            </div>
         </div>
     )
 }
