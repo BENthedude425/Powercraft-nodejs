4c29bfedbcb93cc2ec5eb377fbe582c9051038c7
BENthedude425 <unconfigured@null.spigotmc.org>
2024-07-21 16:34:04 +0100

    user request system setup

---
+++ backend/create-user.jsx
@@ -0,0 +1,72 @@
+import { React } from "react";
+import "../src/assets/main.css";
+import GetAPIAddr from "../src/assets/getAPIAddr";
+import SendForm from "../src/assets/sendForm"
+
+function Pcreate_user() {
+    const APIADDR = GetAPIAddr();
+    return (
+        <div className="page" style={{ display: "flex" }}>
+            <div className="container_1">
+                <div className="container_header">
+                    <div style={{ display: "inline" }}>
+                        <span>Send user request</span>
+                    </div>
+
+                    <img
+                        src="../pictures/apple.png"
+                        className="container-logo"
+                    />
+                </div>
+
+                <form id="creation-form">
+                    <input
+                        type="text"
+                        name="username"
+                        className="text_input"
+                        placeholder="username"
+                    />
+
+                    <input
+                        type="password"
+                        name="password"
+                        className="text_input"
+                        placeholder="password"
+                        required
+                    />
+
+                    <input
+                        type="password"
+                        name="password2"
+                        className="text_input"
+                        placeholder="re-type password"
+                        required
+                    />
+
+                    <input
+                        type="button"
+                        onClick={() => {
+                            SendForm("creation-form", "/api/create-user")
+                        }}
+                        value="Create account"
+                        className="create-submit"
+                    />
+
+                    <div>
+                        <button
+                            className="bottom"
+                            type="button"
+                            onClick={() => {
+                                window.location = "/login";
+                            }}
+                        >
+                            Login
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default Pcreate_user;
---
+++ frontend/src/assets/sendForm.jsx
@@ -0,0 +1,45 @@
+import GetAPIAddr from "./getAPIAddr";
+
+function GatherForm(formID) {
+    const form = new FormData();
+    const formElement = document.getElementById(formID);
+    const children = formElement.children;
+
+    for (const child of formElement.children) {
+        if (child.tagName == "INPUT") {
+            const name = child.name;
+            const value = child.value;
+            form.append(name, value);
+        }
+    }
+
+    return form;
+}
+
+function SendForm(formID, formPath) {
+    const APIADDR = GetAPIAddr();
+    const formData = GatherForm(formID);
+    const xhr = new XMLHttpRequest();
+    console.log(`${APIADDR}${formPath}`);
+
+    xhr.open("POST", `${APIADDR}${formPath}`);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send(new URLSearchParams(formData));
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            const response = xhr.responseText;
+            const responseJSON = JSON.parse(response);
+            console.log(responseJSON)
+            if (responseJSON[0]) {
+                alert(responseJSON[1])
+                window.location = responseJSON[2];
+                return;
+            }
+
+            alert(responseJSON[1]);
+        }
+    };
+}
+
+export default SendForm;
